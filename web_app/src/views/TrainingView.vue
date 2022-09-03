aa21e72e1c6404d36be6e0fc4e4819375682895a
MohammedDev315 <mmohammaddev33@gmail.com>
2022-09-03 12:01:13 +0300

    QR code page  added

--- web_app/src/views/TrainingView.vue
+++ web_app/src/views/TrainingView.vue
@@ -1,13 +1,23 @@
 <template>
-  <h1>Training</h1>
+  <div>
+    <vue-qrcode 
+    v-bind:value="qrValue"
+    v-bind:scale="qrScale"/>
+  </div>
 </template>
-
+ 
 <script>
+import VueQrcode from 'vue-qrcode'
 export default {
 
+  components:{
+   VueQrcode
+  },
+  data(){
+    return{
+      qrValue : "Welcome to vue js",
+      qrScale: 15
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
